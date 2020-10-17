e2cff9cf3813a11ffe680d5d82d1cd76d3a6fde5
rohit-px2 <rohit.p.pradhan88@gmail.com>
2020-10-16 21:35:54 -0400

    Cleared up unused imports

--- core/src/com/rprad/game/FlashDash.java
+++ core/src/com/rprad/game/FlashDash.java
@@ -1,11 +1,8 @@
 package com.rprad.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlashDash extends Game {
